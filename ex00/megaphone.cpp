0a6db3dc85a44ca1ac59d9bd99eaf1c6f55c2720
Satoru Komatsu <skomatsu@c3r3s3.42tokyo.jp>
2025-09-18 22:58:35 +0900

    first commit

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skomatsu <skomatsu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 19:37:24 by skomatsu          #+#    #+#             */
+/*   Updated: 2025/09/18 20:45:11 by skomatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main (int argc, char **argv)
+{
+    if (argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+        {
+            std::string str(argv[i]);
+            for (int j = 0; j < str.length(); j++)
+                std::cout << (char)std::toupper(str[j]);
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
